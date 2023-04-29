36cc100544d5f1fe7457678d387922de245815a8
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2023-04-29 10:26:13 +0100

    Implemented Slideshow within Gallery Section

---
+++ js/slideshow.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("imageSlideShow");
+  let dots = document.getElementsByClassName("dot-display");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-dot";
+  setTimeout(showSlides, 10000);
+}
