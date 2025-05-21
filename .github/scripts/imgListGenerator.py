ab007d608d6cc900b8def1158cafd20b4f572ae5
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-05-21 10:11:11 +0100

    Website redesign

---
+++ .github/scripts/imgListGenerator.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+def pullFiles(path):
+    file_list = []
+    for _, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(('.png', '.jpg', '.JPG', '.jpeg', '.gif')):
+                file_list.append(file)
+    return file_list
+
+retrievedFileList=pullFiles('img/gallery/')
+print(retrievedFileList)
+
+# Output to a specific location
+output_path = 'img/gallery/GalleryList.json'
+with open(output_path, 'w') as f:
+    json.dump(retrievedFileList, f, indent=2)
+    f.close
